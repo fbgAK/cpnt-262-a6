ef787d18b0d038721ea329f45b92280e3dc3c3fd
Akash <147472633+fbgAK@users.noreply.github.com>
2023-11-16 18:20:02 -0700

    Update FeatureComponent.svelte

--- src/routes/FeatureComponent.svelte
+++ src/routes/FeatureComponent.svelte
@@ -1,17 +1,22 @@
-
 <script>
+        // prop => title
     export let title;
     import { createEventDispatcher } from 'svelte';
   
     // Step 5
+    // Create EventDispatcher 
     const dispatch = createEventDispatcher();
-  
+  // Dispatch featureEvent
     function triggerFeatureEvent() {
         dispatch('featureEvent');
     }
 </script>
   
 <div>
-    <h2>{title}</h2>
+
+    <!-- render title prop in header 2 -->
+ <h2>{title}</h2>
+ 
+     <!-- feature event button -->
     <button on:click={triggerFeatureEvent}>Feature Event</button>
 </div>
